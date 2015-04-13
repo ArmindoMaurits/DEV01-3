c275a90d4b0f3f259fcc32d6771cdf95e82395d7
ArmindoMaurits <a.r.maurits@gmail.com>
2015-04-13 11:53:44 +0200

    Show alle data in applicatie, maar nog niet heel netjes uitgelijnd.

--- src/opdracht2/Window.java
+++ src/opdracht2/Window.java
@@ -2,6 +2,7 @@ package opdracht2;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -10,21 +11,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import opdracht2.Persoon;
 
-import java.util.Arrays;
-import java.util.Vector;
-
-/**
- * Created by Armindo on 29-Mar-15.
- */
 public class Window{
     private Persoon[] personen;
     private JFrame frame;
     private JPanel panel;
-    private JButton button;
-    private JLabel label;
+    private JLabel knownForLabel;
+    private JLabel birthRangeLabel;
     private JList<opdracht2.Persoon> list;
     private JLabel imageLabel;
 
@@ -35,25 +28,36 @@ public class Window{
     public void start() {
         frame = new JFrame("Known pioneers");
         frame.setVisible(true);
-        frame.setSize(1500, 800);
+        frame.setSize(1000, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         panel = new JPanel(new BorderLayout());
+        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
         panel.setBackground(Color.DARK_GRAY);
-        panel.setSize(1500, 800);
+        panel.setSize(1000, 800);
 
         leesPersonenIn();
         list = new JList<opdracht2.Persoon>();
         list.setListData(personen);
 
-        panel.add(list, BorderLayout.WEST);
         imageLabel = new JLabel();
-        imageLabel.setSize(200,200);
-        panel.add(imageLabel, BorderLayout.EAST);
+        imageLabel.setSize(400,300);
+        knownForLabel = new JLabel();
+        knownForLabel.setForeground(Color.WHITE);
+        birthRangeLabel = new JLabel();
+        birthRangeLabel.setForeground(Color.WHITE);
+
+        panel.add(list, BorderLayout.WEST);
+        panel.add(imageLabel, BorderLayout.CENTER);
+        panel.add(knownForLabel, BorderLayout.CENTER);
+        panel.add(birthRangeLabel, BorderLayout.EAST);
+
         list.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 updateImage(list.getSelectedValue());
+                updateKnownFor(list.getSelectedValue());
+                updateBirthRange(list.getSelectedValue());
             }
         });
 
@@ -70,13 +74,33 @@ public class Window{
         }
     }
 
+    public void updateKnownFor(Persoon persoon) {
+        try {
+            knownForLabel.setText(persoon.getKnownFor());
+            knownForLabel.updateUI();
+            System.out.println(persoon.getKnownFor());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void updateBirthRange(Persoon persoon) {
+        try {
+            birthRangeLabel.setText(persoon.getBirthrange());
+            birthRangeLabel.updateUI();
+            System.out.println(persoon.getKnownFor());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public void leesPersonenIn() {
         String fileToParse = "./data/pioneers.csv";
         BufferedReader fileReader = null;
         final String DELIMITER = ",";
 
         try {
-            String line = "";
+            String line;
             fileReader = new BufferedReader(new FileReader(fileToParse));
             personen = new Persoon[1];
             while ((line = fileReader.readLine()) != null)
